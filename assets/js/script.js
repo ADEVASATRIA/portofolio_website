81f5c38747228a782bbdbae4586b727befd3931a
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-08-18 00:24:44 +0700

    [Updated]Updated Title, About Me, Experience,& Not Complete Project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-function tooglemenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburgers-icon");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// function typingtext(){
+//     let typingtext = new Typed('title', {
+//         strings: ['Adeva Satria Arif Wibawa', 'Students of Computer Science'],
+//         loop: true,
+//         typeSpeed: 100,
+//         backSpeed: 50,
+//         backDelay: 1000,
+//     })
+// }
+
+// var typed = new typed('#title', {
+//     strings: ['Teks pertama.', 'Teks kedua.', 'Teks ketiga.'],
+//     typeSpeed: 50
+// });
+
+let typingText = new Typed('#text', {
+    strings: ['Adeva Satria Arif Wibawa', 'Web Developer', 'Front End Developer', 'Back End Developer'],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+})
